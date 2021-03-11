4c49e670b70391b1ba849de67d4e53d9b1c8744c
zainmalik65 <zenimalik234@gmail.com>
2021-03-11 22:10:36 +0500

    lec 46 completed

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,36 @@
-import React,{useEffect}from 'react'
+import React,{Fragment,useEffect}from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux';
+import Spinner from '../layout/Spinner';
 import {getCurrentProfile} from '../../actions/profile';
-const Dashboard = ({getCurrentProfile,auth,profile}) => {
+import { Link } from 'react-router-dom';
+const Dashboard = ({
+    getCurrentProfile,
+    auth:{user},
+    profile:{profile,loading}}) => {
     useEffect(()=>{
         getCurrentProfile();
     },[]);
-    return <div>Dashboard</div>
+    return loading && profile== null? (
+    <Spinner/>
+    ):(
+    <Fragment>
+        <h1 className="large text-primary"> Dashboard</h1>
+        <p className="lead">
+        <i className="fas fa-user"></i> Welcome {user && user.name}
+        </p>
+        {profile!==null?(
+        <Fragment>has</Fragment>
+        ):(
+            <Fragment>
+                <p>You have not yet set a profile,please add some info </p>
+                <Link to='create-profile' className='btn btn-primary my-1'>Create Profile</Link>
+            </Fragment>
+        )
+
+        }
+    </Fragment>
+    );
     
 };
 
@@ -25,20 +49,3 @@ Dashboard.propTypes = {
   export default connect(mapStateToProps, { getCurrentProfile })(
     Dashboard
   );
-/*
-Dashboard.propTypes = {
-getCurrentProfile:PropTypes.func.isRequired,
-auth:PropTypes.object.isRequired,
-profile:PropTypes.object.isRequired 
-}
-
-const mapStateToProps=state=>({
-
-    auth:state.auth,
-    profile:state.profile
-    
-});
-
-export default connect(mapStateToProps,{getCurrentProfile})(Dashboard);
-
-*/
